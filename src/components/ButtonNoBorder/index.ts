8bfe1c743454ce5800d2af757ef62dc9658db086
João Pedro <pedrobarros2010@gmail.com>
2021-06-04 16:32:15 -0300

    feat: create button no border styled component

---
+++ src/components/ButtonNoBorder/index.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+
+  margin: 5rem auto 2rem;
+
+  svg {
+    margin-left: 1rem;
+  }
+`
