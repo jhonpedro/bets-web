bd9bfa668c8d9b581d5b005bbe4ecb2ece05785e
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 16:34:46 -0300

    feat: add redux persist and dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import Routes from './routes'
+import { PersistGate } from 'redux-persist/integration/react'
 
+import Routes from './routes'
 import GlobalStyle from './assets/global'
 import store from './store'
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes />
-        <GlobalStyle />
-      </BrowserRouter>
+    <Provider store={store.root}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <BrowserRouter>
+          <Routes />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const persistedReducer = persistReducer({ key: '@tgl', storage }, rootReducer)
 
-export default store
+const store = createStore(persistedReducer, composeWithDevTools())
+
+// @ts-ignore
+export default { root: store, persistor: persistStore(store) }
 
 export type RootState = ReturnType<typeof store.getState>
