a2930c9d60e38158ccd590c26f3ad2ef340d025a
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 10:46:49 -0300

    feat: create singup page

---
+++ src/pages/SingUp/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BrandSlogan from '../../components/BrandSlogan'
+import SingUp from '../../components/BoxInputs/SingUp'
+
+const SingUpPage = () => (
+  <BrandSlogan>
+    <SingUp />
+  </BrandSlogan>
+)
+
+export default SingUpPage
