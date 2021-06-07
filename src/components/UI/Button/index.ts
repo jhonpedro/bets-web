69df87eb86c86def6a3456ba12facd0408695e75
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 16:28:25 -0300

    feat: create base button no border

---
+++ src/components/UI/Button/index.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const ButtonNoBorder = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  color: inherit;
+  font: inherit;
+
+  svg {
+    margin-left: 1rem;
+  }
+`
