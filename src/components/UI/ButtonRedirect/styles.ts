52e79c9d1f3c5ccffc4489a631daa28f981ed558
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 16:34:15 -0300

    feat: use base button no border

--- src/components/UI/ButtonRedirect/styles.ts
+++ src/components/UI/ButtonRedirect/styles.ts
@@ -1,18 +1,9 @@
 import styled from 'styled-components'
+import { ButtonNoBorder } from '../Button'
 
-const ButtonNoBorderStyled = styled.button`
-  display: flex;
-  align-items: center;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-
+const ButtonNoBorderStyled = styled(ButtonNoBorder)`
   margin: 5rem auto 2rem;
   font-size: 3.5rem;
   font-weight: bold;
-
-  svg {
-    margin-left: 1rem;
-  }
 `
 export default ButtonNoBorderStyled
