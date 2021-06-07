8b0a27e0e8465e747e6e8d2d0b75df81fb026335
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 10:47:29 -0300

    feat: add sing-in and forgot password routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import ForgotPassword from '../pages/ForgotPassword'
 import Login from '../pages/Login'
+import SingUp from '../pages/SingUp'
 
 function Routes() {
   return (
     <Switch>
       <Route path="/login">
         <Login />
+      </Route>
+      <Route path="/sing-in">
+        <SingUp />
+      </Route>
+      <Route path="/forgot-password">
+        <ForgotPassword />
       </Route>
     </Switch>
   )
