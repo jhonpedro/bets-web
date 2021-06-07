ce35a2ffc892c8a7e98da54f1885f803f67b79da
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 10:42:45 -0300

    feat: create container for our inputs box

--- src/components/BoxInputs/styles.ts
+++ src/components/BoxInputs/styles.ts
@@ -1,8 +1,39 @@
 import styled from 'styled-components'
+import ButtonRedirect from '../ButtonRedirect/styles'
 import { InputContainer } from '../Input/styles'
 
-const BoxInputsContainer = styled.form`
+export const BoxInputsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  strong {
+    font-size: 3.5rem;
+    color: var(--color-dark);
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  ${ButtonRedirect} {
+    color: var(--color-dark);
+    margin: 4rem auto 1rem;
+  }
+`
+
+const BoxInputsBox = styled.form`
   padding: 2rem;
+  width: 50%;
+  overflow: hidden;
+  border-radius: var(--roundness-2);
+  background-color: white;
+  box-shadow: 0 0 1rem var(--color-box-shadow);
+  border: 1px solid var(--color-box-line-op);
+
+  ${ButtonRedirect} {
+    color: var(--color-green);
+  }
 
   ${InputContainer} {
     font-weight: bold;
@@ -14,7 +45,7 @@ const BoxInputsContainer = styled.form`
     margin-top: 2rem;
   }
 
-  > ${InputContainer}:after {
+  ${InputContainer}:after {
     content: '';
     display: block;
     position: absolute;
@@ -24,7 +55,11 @@ const BoxInputsContainer = styled.form`
     bottom: 0;
     left: -2rem;
     right: 0;
+    transition: 300ms ease;
+  }
+  ${InputContainer}.invalid:after {
+    background-color: var(--color-red);
   }
 `
 
-export default BoxInputsContainer
+export default BoxInputsBox
