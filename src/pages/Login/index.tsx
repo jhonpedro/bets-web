4966ac5c5b24fb9f897e2d9218891ea9895e7599
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 10:46:35 -0300

    feat: adapt to new base style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import BoxInputs from '../../components/BoxInputs/Login'
+import Login from '../../components/BoxInputs/Login'
 import BrandSlogan from '../../components/BrandSlogan'
-import { LoginBox, LoginBoxContainer, LoginCointainer } from './styles'
 
 function LoginPage() {
   return (
-    <LoginCointainer>
-      <BrandSlogan />
-      <LoginBoxContainer>
-        <LoginBox>
-          <BoxInputs />
-        </LoginBox>
-      </LoginBoxContainer>
-    </LoginCointainer>
+    <BrandSlogan>
+      <Login />
+    </BrandSlogan>
   )
 }
 
