7f2163ebc844a6dec279d5dd3424d4dd0629f07b
João Pedro <pedrobarros2010@gmail.com>
2021-06-07 10:45:58 -0300

    feat: create forgot password page

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ForgotPassword from '../../components/BoxInputs/Password'
+import BrandSlogan from '../../components/BrandSlogan'
+
+const ForgotPasswordPage = () => (
+  <BrandSlogan>
+    <ForgotPassword />
+  </BrandSlogan>
+)
+
+export default ForgotPasswordPage
