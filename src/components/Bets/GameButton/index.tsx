ce225e8a5dcbbb792d385a9e2723186b532e6bf9
João Pedro <pedrobarros2010@gmail.com>
2021-06-08 11:36:01 -0300

    feat: create game button

---
+++ src/components/Bets/GameButton/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { GameButtonElement } from './styles'
+
+interface GameButtonProps {
+  title: string
+  color: string
+  isActive: boolean
+  // eslint-disable-next-line no-unused-vars
+  setCurrentGame: (gameType: string) => void
+}
+
+const GameButton: React.FC<GameButtonProps> = ({
+  title,
+  color,
+  isActive,
+  setCurrentGame,
+}) => {
+  const handleSetNewCurrentGame = () => {
+    setCurrentGame(title)
+  }
+
+  return (
+    <GameButtonElement
+      color={color}
+      isActive={isActive}
+      onClick={handleSetNewCurrentGame}
+    >
+      {title}
+    </GameButtonElement>
+  )
+}
+
+export default GameButton
---
+++ src/components/Bets/GameButton/styles.ts
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components'
+
+interface GameButtonProps {
+  isActive: boolean
+  color: string
+}
+
+export const GameButtonElement = styled.button<GameButtonProps>`
+  padding: 0.5rem 2rem;
+  background-color: white;
+  font-style: italic;
+  border: 2px solid ${(props) => props.color};
+  border-radius: 2rem;
+  transition: 300ms ease;
+
+  ${(props) => {
+    if (props.isActive) {
+      return css`
+        color: white;
+        background-color: ${props.color};
+      `
+    }
+
+    return css`
+      background-color: white;
+      color: ${props.color};
+    `
+  }}
+`
