b94a597abc7a477d2127aaaec7c7adee74be5afe
João Pedro <pedrobarros2010@gmail.com>
2021-06-08 11:37:56 -0300

    feat: create new bet page and fallback redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Switch } from 'react-router-dom'
+import { Redirect, Switch } from 'react-router-dom'
 
 import Route from './Route'
 import ForgotPassword from '../pages/ForgotPassword'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
 import SingUp from '../pages/SingUp'
+import NewBet from '../pages/NewBet'
 
 function Routes() {
   return (
@@ -21,6 +22,12 @@ function Routes() {
       </Route>
       <Route path="/forgot-password">
         <ForgotPassword />
+      </Route>
+      <Route path="/new-bet" isPrivate>
+        <NewBet />
+      </Route>
+      <Route path="*">
+        <Redirect to="/" />
       </Route>
     </Switch>
   )
