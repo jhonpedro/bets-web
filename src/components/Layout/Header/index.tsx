8767ea03ef3d4c955c7d83ede146591b7ae18c99
João Pedro <pedrobarros2010@gmail.com>
2021-06-09 16:45:57 -0300

    feat: add redirect on click in TGL and logout function

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import { FiLogOut } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Link, useHistory } from 'react-router-dom'
+import { actionLogout } from '../../../store/reducers/auth/actions'
 import { ButtonNoBorder } from '../../UI/Button'
 import { Actions, BrandHeading, HeaderContainer } from './styles'
 
-const Header = React.memo(() => (
-  <HeaderContainer className="container">
-    <BrandHeading>TGL</BrandHeading>
-    <Actions>
-      <Link to="/profile">Account</Link>
-      <ButtonNoBorder>
-        Logout
-        <FiLogOut />
-      </ButtonNoBorder>
-    </Actions>
-  </HeaderContainer>
-))
+const Header = React.memo(() => {
+  const dispatch = useDispatch()
+  const { push } = useHistory()
+
+  const handleLogout = () => {
+    dispatch(actionLogout())
+    push('/login')
+  }
+
+  return (
+    <HeaderContainer className="container">
+      <BrandHeading>
+        <Link to="/">TGL</Link>
+      </BrandHeading>
+      <Actions>
+        <Link to="/account">Account</Link>
+        <ButtonNoBorder onClick={handleLogout}>
+          Logout
+          <FiLogOut />
+        </ButtonNoBorder>
+      </Actions>
+    </HeaderContainer>
+  )
+})
 
 export default Header
