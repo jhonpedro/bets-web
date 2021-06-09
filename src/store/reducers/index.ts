d5065dd6979708b6e2b8107753b86be6de4ddb5e
João Pedro <pedrobarros2010@gmail.com>
2021-06-09 16:47:26 -0300

    feat: add new cart reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth/reducer'
+import cartReducer from './cart/reducer'
 import usersReducer from './users/reducer'
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  cart: cartReducer,
   users: usersReducer,
 })
 
