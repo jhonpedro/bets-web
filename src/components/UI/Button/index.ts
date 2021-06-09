667e2ef1bddc4d9d04bf71f2c22c98d0332787fa
João Pedro <pedrobarros2010@gmail.com>
2021-06-09 16:46:13 -0300

    feat: create outline button

--- src/components/UI/Button/index.ts
+++ src/components/UI/Button/index.ts
@@ -9,6 +9,29 @@ export const ButtonNoBorder = styled.button`
   font: inherit;
 
   svg {
-    margin-left: 1rem;
+    margin: 0 1rem;
+  }
+`
+
+export const ButtonOutline = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background-color: transparent;
+  color: ${(props) => props.color};
+  font: inherit;
+  border: 2px solid ${(props) => props.color};
+  transition: 300ms ease;
+  padding: 1rem 2rem;
+  border-radius: var(--roundness-1);
+
+  :hover {
+    background-color: ${(props) => props.color};
+    color: white;
+  }
+
+  svg {
+    margin-right: 1rem;
+    font-size: 2rem;
   }
 `
