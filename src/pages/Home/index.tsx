a46445b0f6b2be19df925177061a379470ede28d
João Pedro <pedrobarros2010@gmail.com>
2021-06-09 16:51:50 -0300

    feat: add recent games list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,39 @@
 import React from 'react'
 import { FiArrowRight } from 'react-icons/fi'
+import RecentGameList from '../../components/Landing/RecentGameList'
 import Header from '../../components/Layout/Header'
 import ButtonRedirect from '../../components/UI/ButtonRedirect'
+import useGetRecentGames from '../../store/selectors/users/useGetRecentGames'
 import { Filters, HomeContainer, HomeHeaderActions } from './styles'
 
-const Home = () => (
-  <>
-    <Header />
-    <HomeContainer className="container">
-      <HomeHeaderActions>
-        <h3>RECENT GAMES</h3>
-        <Filters>
-          <span>Filters</span>
-          <div>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-          </div>
-        </Filters>
+const Home = () => {
+  const recentgames = useGetRecentGames()
 
-        <ButtonRedirect goTo="/new-bet">
-          New Bet
-          <FiArrowRight />
-        </ButtonRedirect>
-      </HomeHeaderActions>
-    </HomeContainer>
-  </>
-)
+  return (
+    <>
+      <Header />
+      <HomeContainer className="container">
+        <HomeHeaderActions>
+          <h3>RECENT GAMES</h3>
+          <Filters>
+            <span>Filters</span>
+            <div>
+              <span>1</span>
+              <span>2</span>
+              <span>3</span>
+            </div>
+          </Filters>
+
+          <ButtonRedirect goTo="/new-bet">
+            New Bet
+            <FiArrowRight />
+          </ButtonRedirect>
+        </HomeHeaderActions>
+
+        {recentgames ? <RecentGameList items={recentgames} /> : null}
+      </HomeContainer>
+    </>
+  )
+}
 
 export default Home
