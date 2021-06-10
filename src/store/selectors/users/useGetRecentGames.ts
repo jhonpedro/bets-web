eadd1a23f2bf245307818555dc8870f17796bc93
João Pedro <pedrobarros2010@gmail.com>
2021-06-10 10:39:10 -0300

    fix: ensure array return even if current user isn't found

--- src/store/selectors/users/useGetRecentGames.ts
+++ src/store/selectors/users/useGetRecentGames.ts
@@ -1,9 +1,14 @@
 import useAppSelector from '../../../hooks/useAppSelector'
 
 const useGetRecentGames = () =>
-  useAppSelector(
-    (state) =>
-      state.users.find((user) => user.email === state.auth.email)?.recentGames
-  )
+  useAppSelector((state) => {
+    const currentUser = state.users.find(
+      (user) => user.email === state.auth.email
+    )
+    if (!currentUser) {
+      return []
+    }
+    return currentUser.recentGames
+  })
 
 export default useGetRecentGames
