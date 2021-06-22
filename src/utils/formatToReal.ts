060617041ea8aa57b5f4bba81aaa68e7ca6e23d6
João Pedro <pedrobarros2010@gmail.com>
2021-06-22 16:23:01 -0300

    fix: add comma separator and limit n2 to 2 characters

--- src/utils/formatToReal.ts
+++ src/utils/formatToReal.ts
@@ -2,9 +2,13 @@ const formatToReal = (numberToFormat: number): string => {
   const numberToFormatString = numberToFormat.toString()
 
   return numberToFormatString.replace(
-    /(\d+)(?:.(\d+))?/,
+    /(\d+)(?:.(\d{1,2}))?/,
     (_, n1: string, n2: string) =>
-      `${n1}${!n2 ? '.00' : `.${n2.length === 1 ? `${n2}0` : `${n2}`}`}`
+      `${n1}${
+        !n2
+          ? ',00'
+          : `,${n2.length === 1 ? `${n2}0` : `${n2.toString().slice(0, 1)}`}`
+      }`
   )
 }
 
