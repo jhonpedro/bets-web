44b8bf5b01b6caf667cc1c9f16bb13a17e43b454
João Pedro <pedrobarros2010@gmail.com>
2021-06-22 16:33:34 -0300

    feat: remove users reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth/reducer'
 import cartReducer from './cart/reducer'
-import usersReducer from './users/reducer'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   cart: cartReducer,
-  users: usersReducer,
 })
 
 export default rootReducer
